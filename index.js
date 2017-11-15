fe5885a471ba1f9690032af50fe9ae9afadaad8f
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-11-16 03:05:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => 
+  <div>
+    <h1>Cowsay</h1>
+    <hr />
+  </div>
+
+ReactDOM.render(<App />, document.getElementById('app'))
